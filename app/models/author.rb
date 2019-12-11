6dc4218c7bd01bab4a88f22b5284ca3087dfdba7
Manohar Shetty <manohar.shetty@grabtaxi.com>
2019-12-11 17:17:56 +0800

    extract common code

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,17 +1,4 @@
 class Author < ApplicationRecord
-
-  def self.from_string(author_string)
-    if author_string.include?("[")
-      delimiters = [/(?<=\>),\s/, /(?<=\]),\s/]
-      authors = author_string.split(Regexp.union(delimiters))
-    else
-      authors = author_string.split(",")
-    end
-    return [] unless authors
-    authors.map do |author|
-      email = author[/.*<([^>]*)/,1]
-      name = author.split("<").first.strip
-      Author.new(name: name, email: email)
-    end
-  end
+  include Identity
+  belongs_to :package
 end
---
+++ app/models/concerns/identity.rb
@@ -0,0 +1,22 @@
+module Identity
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def from_string(identity_string)
+      if identity_string.include?("[")
+        delimiters = [/(?<=\>),\s/, /(?<=\]),\s/]
+        identities = identity_string.split(Regexp.union(delimiters))
+      else
+        identities = identity_string.split(",")
+      end
+      return [] unless identities
+      identities.map do |identity|
+        email = identity[/.*<([^>]*)/,1]
+        name = identity.split("<").first.strip
+        self.new(name: name, email: email)
+      end
+    end
+
+  end
+
+end
